1cc1c857b892f4f5e9c7317a36ab56a7787a5344
Ryan Cohen <47368809+ryanjcohen@users.noreply.github.com>
2020-05-29 20:29:10 -0400

    Added bot ability to respond to tagged messages.
    Now, when the bot is tagged in a message in a channel to which it has been added, if link(s) and tagged user(s) are also present in the message, the bot will add the link(s) to the reading list(s) of the tagged user(s). If at least one link or at least one tagged user other than the bot are not present in the message in which the bot is tagged, the bot will reply with an appropriate error message.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from slackeventsapi import SlackEventAdapter
 import os
 import logging
 import pymongo
+from urllib.parse import urlparse
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 slack_web_client = WebClient(SLACK_BOT_TOKEN)
@@ -95,22 +96,70 @@ def post_message(channel, message):
 
 @slack_events_adapter.on("message")
 def handle_message(event_data):
-    message = event_data["event"]
-    text = message["text"]
-    message_words = text.split()
-    if message.get("bot_id") is None:
-    	channel = message["channel"]
-    	client_id = message["user"] + message["team"]
-    	if message_words[0].lower() == "add":
-    		add_link(client_id, channel, message_words)
-    	elif message_words[0].lower() == "view":
-    		view_links(client_id, channel, message_words)
-    	elif message_words[0].lower() == "remove":
-    		remove_link(client_id, channel, message_words)
-    	elif message_words[0].lower() == "clear":
-    		clear_list(client_id, channel)
-    	elif message.get("subtype") is None:
-	        post_message(channel,instructions)
+	message = event_data["event"]
+	if message.get("bot_id") is None and message.get("subtype") is None:
+		text = message["text"]
+		message_words = text.split()
+		channel = message["channel"]
+		client_id = message["user"] + message["team"]
+		if message_words[0].lower() == "add":
+			add_link(client_id, channel, message_words)
+		elif message_words[0].lower() == "view":
+			view_links(client_id, channel, message_words)
+		elif message_words[0].lower() == "remove":
+			remove_link(client_id, channel, message_words)
+		elif message_words[0].lower() == "clear":
+			clear_list(client_id, channel)
+		elif message.get("subtype") is None:
+			post_message(channel,instructions)
+	#TODO: handle subtype message_changed?
+
+@slack_events_adapter.on("app_mention")
+def handle_mention(event_data):
+	data = event_data['event']['blocks'][0]['elements'][0]['elements']
+	ts = event_data['event']['ts']
+	team = event_data['team_id']
+	user_ids = []
+	links = []
+	for element in data:
+		if element['type']=='user':
+			user_ids.append(element['user_id'])
+		elif element['type']=='link':
+			links.append(element['url'])
+	channel = event_data['event']['channel']
+	if len(links)==0:
+		no_links = "Message does not contain a valid link."
+		slack_web_client.chat_postMessage(channel=channel,text=no_links,thread_ts=ts)
+	elif len(user_ids)==1:
+		no_users = "Message does not indicate which user reading lists to add link to."
+		slack_web_client.chat_postMessage(channel=channel,text=no_users,thread_ts=ts)
+	else:
+		names = []
+		for user in user_ids:
+			user_info = slack_web_client.users_profile_get(user=user)
+			name = user_info['profile']['real_name']
+			if name != 'ReadingLinks':
+				names.append(name)
+				client_id = user + team
+				result = users.find_one({"_id":client_id})
+				for idx in range(len(links)):
+					link = links[idx]
+					if idx==0 and users.count_documents({"_id":client_id}) == 0:
+						users.insert_one({"_id": client_id, "list":[link]})
+					elif link not in result['list']:
+						users.update_one({"_id":client_id}, {"$push":{"list":link}})
+		added_message = ""
+		if len(links)>1 and len(names)>1:
+			added_message = "The above links are now in the reading lists of:"
+		elif len(links)>1 and len(names)==1:
+			added_message = "The above links are now in the reading list of:"
+		elif len(links)==1 and len(names)>1:
+			added_message = "The above link are now in the reading lists of:"
+		elif len(links)==1 and len(names)==1:
+			added_message = "The above link are now in the reading list of:"
+		for name in names:
+			added_message = added_message + " @" + name
+		slack_web_client.chat_postMessage(channel=channel,text=added_message,thread_ts=ts)
 
 @slack_events_adapter.on("error")
 def error_handler(err):
